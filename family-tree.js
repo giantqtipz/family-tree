f7b7ca35e81caff19e15cc9ec89a5276fccada1b
Bima Saridjo <palessie27@gmail.com>
2020-04-24 19:37:05 -0400

    first attempt

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,34 @@
 class FamilyTree {
-  constructor () {}
+  constructor (value){
+    if(typeof value !== 'string'){
+      throw 'error';
+    } else {
+      this.value = value;
+    }
+
+    this.children = [];
+  }
+  
+  insert(name){
+    this.children.push(name);
+    return this.children;
+  }
+
+  familySize(){
+    return this.children.length + 1;
+  }
+
+  findMember(name){
+    if(this.value.includes(name)){
+      return this.children;
+    } else {
+      return 'undefined';
+    }
+  }
+
+  log(){
+
+  }
 }
 
 module.exports = FamilyTree;
