869d38437632a93bf3a32352a8762ebfd5e533ce
Bima Saridjo <palessie27@gmail.com>
2020-04-27 08:51:09 -0400

    cleaned up code and comments

--- family-tree.js
+++ family-tree.js
@@ -24,27 +24,33 @@ class FamilyTree {
 
     //  Take 1 - Initially failed the test spec, because the forEach method is returning inside the method. 
     //  Learned not to return 'index" inside the method, but instead reassign a variable with the value of 'index'.
-      let person = '';
+      
+    let person = '';
       this.children.forEach(function(index){
         if(index.value === name){
           person = index;
         }
       });
-      return person;
+    return person;
 
+    //  ----------
     //  Take 2 - This is accepted as is, and will solve all test specs. 
     //  But learned not to return inside a for loop. 
+    
     //  for (let i = 0; i < this.children.length; i++) {
     //    if (this.children[i].value === name) {
     //      return this.children[i];
     //    } 
     //   } 
 
+    //  ----------
     //  Take 3 - This passes this test spec, but will cause log test spec to fail 
     //  (Henry and Vivian will be Mike's children, not Amy's). This is because spread operators do not loop through arrays.
+    
     //  return function(arg){
     //    return arg;
     //  }(...this.children);
+    
   }
 
   log(){
