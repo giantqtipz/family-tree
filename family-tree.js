6af582ce93136fdfb44fa5fe1a8c1dc53a49dbe3
Bima Saridjo <palessie27@gmail.com>
2020-04-27 08:46:58 -0400

    cleaned up code and comments

--- family-tree.js
+++ family-tree.js
@@ -5,7 +5,6 @@ class FamilyTree {
     } else {
       this.value = value;
     }
-
     this.children = [];
   }
   
@@ -23,21 +22,23 @@ class FamilyTree {
 
   findMember(name){
 
-      // Take 1 -- same output as below when console.log(), but not accepted
-      // return this.children.forEach(function(index){
-      //   if(index.value === name){
-      //     console.log(index);
-      //   }
-      // });
+    // Take 1 -- Initially failed the test spec, because the forEach method is returning inside the method. Learned not to return 'index" inside the method, but instead reassign a variable with the value of 'index'.
+      let person = '';
+      this.children.forEach(function(index){
+        if(index.value === name){
+          person = index;
+        }
+      });
+      return person;
 
-      // Take 2 - this is accepted.. and will solve all test specs
-      for (let i = 0; i < this.children.length; i++) {
-        if (this.children[i].value === name) {
-          return this.children[i];
-        } 
-      }       
+      // // Take 2 - this is accepted as is, and will solve all test specs. But learned not to return inside a for loop. 
+      // for (let i = 0; i < this.children.length; i++) {
+      //   if (this.children[i].value === name) {
+      //     return this.children[i];
+      //   } 
+      // } 
 
-      // Take 3 -- this was an accidental solve... this passes this test spec, but will cause log test spec to fail (Henry and Vivian will be Mike's children, not Amy's)
+      // Take 3 -- this passes this test spec, but will cause log test spec to fail (Henry and Vivian will be Mike's children, not Amy's). This is because it's a spread operator, not a method to loop through arrays.
       // return function(arg){
       //   return arg;
       // }(...this.children);
@@ -45,10 +46,10 @@ class FamilyTree {
 
   log(){
       const parent = `-- ${this.value}`;
+      let family = [parent];
       if(this.children.length === 0){
         return parent;
       } else {
-        let family = [parent];
         this.children.map(function(index){
           family.push(`---- ${index.value}`);
           return index.children.map(function(ind){
@@ -61,40 +62,3 @@ class FamilyTree {
 }
 
 module.exports = FamilyTree;
-
-// log(){
-//   const parent = `-- ${this.value}`;
-//   let family = [parent];
-  
-//   if(this.children.length === 0){
-//     return parent;
-//   } else {
-//     return this.children.map(function(index){
-//       family.push(`---- ${index.value}`);
-//       if(index.children.length !== 0){
-//         index.children.map(function(ind){
-//           family.push(`------ ${ind.value}`);
-//         });
-//       }
-//       console.log(family.join('\n'));
-//       // console.log(`${parent} ` + `---- ${current.value} ` + `------ ${current.children} ` + `---- ${next.value} ` + `------ ${next.children} `);
-//     });
-//   }
-// }
-
-
-// log(){
-//   const parent = `-- ${this.value}`;
-//   if(this.children.length === 0){
-//     return parent;
-//   } else {
-//     let family = [parent];
-//     this.children.map(function(index){
-//       family.push(`---- ${index.value}`);
-//       return index.children.map(function(ind){
-//         family.push(`------ ${ind.value}`);
-//       });
-//     });
-//     return family.join('\n');
-//   }
-// }
